7c608c91d8fef1b2864c8026a3c13f28d3bb8bfb
ludtanza <ludtanza@gmail.com>
2022-01-28 14:56:11 +0700

    move script

---
+++ script.js
@@ -0,0 +1,12 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10)
+
+console.log("Tanza");
+console.log(23);
+
+let firstName = "Birru"
+let first = 'Luqi'
+
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
